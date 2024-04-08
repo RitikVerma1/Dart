4cc7409511ed68ad0908d2b806ee2e3579ec37f7
Ritik <ritikverma1912@gmail.com>
2024-04-08 23:13:36 +0530

    changes

---
+++ bin/00test.dart
@@ -0,0 +1,7 @@
+import "dart:io";
+
+void main() {
+  int n = 6;
+
+  for (int i = 0; i < n; i++) {}
+}
--- bin/a14_switchCase.dart
+++ bin/a14_switchCase.dart
@@ -1,3 +1,22 @@
+/* Syntax : 
+
+switch ( expression ) { 
+   case value1: { 
+      // Body of value1
+   } break; 
+   case value2: { 
+      //Body of value2 
+   } break; 
+   .
+   .
+   .
+   default: { 
+      //Body of default case  
+   } break; 
+} 
+
+*/
+
 void main() {
   // in dart we don't to add Break after switch case the the case is not empty.
   // which means is we are not doing some operation in case (not even a basic print) then for that empty cast we need to add break.
