b23199bf4de0c06f7906e2df1aaad933b502f39d
Ritik <ritikverma1912@gmail.com>
2025-03-23 19:32:05 +0530

    updated

--- bin/01coding_chalenges.dart
+++ bin/01coding_chalenges.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:typed_data';
 
 void main() {
   List<dynamic> dList = [10.0, 20, 30, 40, StudentsName("flutter")];
@@ -6,7 +7,15 @@ void main() {
   print(dList[2]);
   dList[4].printInfo();
 
-  Demo dem = Demo("flutter", 200000);
+  dList[0] = 1;
+
+  dList.add(200);
+  dList.insert(0, "0 element");
+
+  List<Demo> dem = [Demo("FL", 23), Demo("DL", 24), Demo("EL", 25)];
+  for (int i = 0; i < dem.length; i++) {
+    print(dem[i]);
+  }
 }
 
 class StudentsName {
@@ -22,7 +31,8 @@ class StudentsName {
 class Demo<T, A> {
   T name;
   A age;
-  Demo(this.name, this.age) {
-    print("$name $age");
-  }
+  Demo(this.name, this.age);
+
+  @override
+  String toString() => "name : $name, age : $age";
 }
--- bin/a56_c_listExtras.dart
+++ bin/a56_c_listExtras.dart
@@ -2,12 +2,16 @@ void main() {
   final item = Stud("ef");
 
   List<Stud> lst = [Stud("ab"), Stud("cd"), item];
+  for (int i = 0; i < lst.length; i++) {
+    print(lst[i]);
+  }
 }
 
 class Stud {
   String name;
   Stud(this.name);
 
+//overriding the default messge when we call instance of a class
   @override
   String toString() => "Name : $name";
 }
