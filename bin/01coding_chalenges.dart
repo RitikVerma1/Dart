69596757c40cca3481ec81ae116e7b3d70d46b20
Ritik <ritikverma1912@gmail.com>
2025-03-30 19:27:28 +0530

    update

--- bin/01coding_chalenges.dart
+++ bin/01coding_chalenges.dart
@@ -1,1 +1,15 @@
+void main() {
+  animal ani = animal(name: "tiger");
+}
 
+class animal {
+  String name;
+
+  animal({required this.name}) {
+    void prinntInfo() {
+      print(name);
+    }
+
+    prinntInfo();
+  }
+}
